32b0e610a0a3b81b9e6390a88118f8283a7bd5ae
jn-97 <kjo5395@gmail.com>
2023-10-10 15:02:36 +0900

    Solution_2068

---
+++ src/D1/Solution_2068.java
@@ -0,0 +1,27 @@
+package D1;
+
+import java.util.Scanner;
+
+public class Solution_2068 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        int arr[] = new int[10];
+
+        for (int tc=1; tc<=T; tc++) {
+            int max = 0;
+
+            for (int i=0; i<10; i++) {
+                arr[i] = sc.nextInt();
+
+                if (arr[i] > max) {
+                    max = arr[i];
+                }
+            }
+
+            System.out.println("#" + tc + " " + max);
+
+        }
+    }
+}
