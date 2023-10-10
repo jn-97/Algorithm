df318bed5356dc9ef9039c3b460e6f5f6fd3d5b4
jn-97 <kjo5395@gmail.com>
2023-10-10 14:08:35 +0900

    Initial commit

---
+++ src/D1/addOddNumbers_2072.java
@@ -0,0 +1,25 @@
+package D1;
+
+import java.util.Scanner;
+
+public class addOddNumbers_2072 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        int[] arr = new int[10];
+
+        for(int tc=1; tc<=T; tc++) {
+            int sum = 0;
+
+            for (int i=0; i<10; i++) {
+                arr[i] = sc.nextInt();
+
+                if (arr[i]%2!=0) {
+                    sum += arr[i];
+                }
+            }
+            System.out.println("#" + tc + " " + sum);
+        }
+    }
+}
