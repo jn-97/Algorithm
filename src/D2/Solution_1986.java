be5305cefeeb9b2e4d856877da581a7652f9195a
jn-97 <kjo5395@gmail.com>
2023-10-28 17:41:29 +0900

    Solution_1986

---
+++ src/D2/Solution_1986.java
@@ -0,0 +1,27 @@
+package D2;
+// 지그재그 숫자
+
+import java.util.Scanner;
+
+public class Solution_1986 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        for (int tc=1; tc<=T; tc++) {
+            int N = sc.nextInt();
+
+            int result = 0;
+            for (int i=1; i<=N; i++) {
+                if (i%2 != 0) {
+                    result += i;
+                } else {
+                    result -= i;
+                }
+            }
+
+            System.out.println("#" + tc + " " + result);
+        }
+    }
+}
