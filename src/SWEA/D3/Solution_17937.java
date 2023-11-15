537b5af0afa9d11683077f01d6308b3983df839e
jn-97 <kjo5395@gmail.com>
2023-11-15 17:03:35 +0900

    Solution_17937

---
+++ src/SWEA/D3/Solution_17937.java
@@ -0,0 +1,34 @@
+package SWEA.D3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Solution_17937 {
+
+    static BigInteger result = BigInteger.valueOf(1);
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int C = Integer.parseInt(br.readLine());
+
+        for (int TC = 1; TC <= C; TC++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            BigInteger A = new BigInteger(st.nextToken());
+            BigInteger B = new BigInteger(st.nextToken());
+
+            GCD(A, B);
+            System.out.println("#" + TC + " " + result);
+        }
+    }
+
+    public static void GCD(BigInteger A, BigInteger B) {
+        if(A.equals(B)) {
+            result = A;
+        }else {
+            result = BigInteger.valueOf(1);
+        }
+    }
+}
